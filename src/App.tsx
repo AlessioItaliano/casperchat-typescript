b2904d9da838ddb34fb12f4f90eaf915d00dc92d
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2024-05-20 21:43:24 +0300

    add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// // import React from "react";
+// // // import logo from './logo.svg';
+// // import "./App.css";
 
-function App() {
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <header className="App-header">
+// //         <img src={logo} className="App-logo" alt="logo" />
+// //         <p>
+// //           Edit <code>src/App.tsx</code> and save to reload.
+// //         </p>
+// //         <a
+// //           className="App-link"
+// //           href="https://reactjs.org"
+// //           target="_blank"
+// //           rel="noopener noreferrer"
+// //         >
+// //           Learn React
+// //         </a>
+// //       </header>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
+
+import { lazy } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+// import PrivateRoute from "./PrivateRoute";
+// import RestrictedRoute from "./RestrictedRoute";
+
+// import Background from "components/Base/Background";
+// // import Loader from 'components/Loader';
+
+// const MainPage = lazy(() => import("pages/Main"));
+// const LoginPage = lazy(() => import("pages/Login"));
+// const RegisterPage = lazy(() => import("pages/Register"));
+// const ChatPage = lazy(() => import("pages/Chat"));
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>test</p>
+    // <Routes>
+    //   <Route path="/" element={<Background />}>
+    //     <Route index element={<MainPage />} />
+    //     <Route
+    //       path="register"
+    //       element={
+    //         <RestrictedRoute redirectTo="/chat" component={RegisterPage} />
+    //       }
+    //     />
+    //     <Route
+    //       path="login"
+    //       element={<RestrictedRoute redirectTo="/chat" component={LoginPage} />}
+    //     />
+    //     <Route
+    //       path="chat"
+    //       element={<PrivateRoute redirectTo="/login" component={ChatPage} />}
+    //     />
+
+    //     <Route path="*" element={<Navigate to="/" />} />
+    //   </Route>
+    // </Routes>
   );
-}
+};
 
 export default App;
